8e866cc99710ffb2076be8b51a8af91107cae4e5
xyzuka <yeebrandon@outlook.com>
2022-05-25 19:02:08 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Superburg
+
+<a href="">👉🏻 Live Demo</a>
+
+<img src="/Images/demo.gif">
+
+#### 📝 Description
+
+Restaurant page project from The Odin Project Curriculum.
+
+#### 💡 Features
+
+- Responsive design
+- Navigation Tabs
+- Accessible form
+
+#### 🛠️ Built with
+
+- HTML
+- CSS
+- JavaScript
+- Webpack5
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: `Superburgs: The best burger spot!`,
+      favicon: 'src/Images/favicon.png',
+      template: 'src/template.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
